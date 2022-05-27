082a935beda079637e52141530269d798aba7d8e
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-05-27 19:15:38 +0300

    class Entity created

---
+++ Dungeons and Dragons/entity.cpp
@@ -0,0 +1,14 @@
+#include "entity.hpp"
+
+Entity::Entity(int _strength, int _mana, int _health) : strength(_strength), mana(_mana), health(_health)
+{}
+
+void Entity::attack(Entity& target)
+{
+    //
+}
+
+void Entity::castSpell(Entity& target)
+{
+    //
+}
---
+++ Dungeons and Dragons/entity.hpp
@@ -0,0 +1,21 @@
+#ifndef __ENTITY_HPP
+#define __ENTITY_HPP
+
+class Entity
+{
+private:
+    int strength;
+    int mana;
+    int health;
+public:
+    Entity(int _strength = 0, int _mana = 0, int _health = 0);
+
+    void attack(Entity&);
+
+    void castSpell(Entity&);
+
+};
+
+
+#endif // !__ENTITY_HPP
+
