88f2eefa01fd18f07b49931f7bd1f0126b4ff501
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-05-28 14:47:24 +0300

    class Mage (subclass of class Race) created

---
+++ Dungeons and Dragons/mage.cpp
@@ -0,0 +1,4 @@
+#include "mage.hpp"
+
+Mage::Mage() : Race("Mage", 10, 40, 50)
+{}
---
+++ Dungeons and Dragons/mage.hpp
@@ -0,0 +1,13 @@
+#ifndef __MAGE_HPP
+#define __MAGE_HPP
+
+#include "race.hpp"
+
+class Mage : public Race
+{
+public:
+    Mage();
+};
+
+
+#endif // !__MAGE_HPP
