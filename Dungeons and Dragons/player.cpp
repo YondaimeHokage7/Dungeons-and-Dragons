68be27e241bc3832380b16607b68307efa1676de
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-05-28 21:41:23 +0300

    Added inventory as member variable and attack, castSpell, takeDamage, printInventory as member functions

--- Dungeons and Dragons/player.cpp
+++ Dungeons and Dragons/player.cpp
@@ -1,10 +1,37 @@
 #include "player.hpp"
+#include <iostream>
 
-Player::Player(Race _race) : race(_race), Entity(_race.getRaceStrength(),_race.getRaceMana(),_race.getRaceHealth())
-{}
+
+Player::Player(Race _race) : race(_race), Entity(_race.getRaceStrength(), _race.getRaceMana(), _race.getRaceHealth())
+{
+    setStrength(0.5 + (getStrength() + (double)inventory.getStrengthModifier() / 100 * getStrength()));
+    setMana(0.5 + (getMana() + (double)inventory.getSpellModifier() / 100 * getMana()));
+    //mana = mana + (double)inventory.getSpellModifier() / 100 * mana;
+    //strength = strength + (double)inventory.getStrengthModifier() / 100 * strength;
+}
 
 std::string Player::getRace() const
 {
     return race.getName();
 }
 
+void Player::attack(Entity& target, int attackDamage)
+{
+    target.takeDamage(0.5 + (attackDamage + inventory.getStrengthModifier() / 100 * attackDamage));
+}
+
+void Player::castSpell(Entity& target, int spellDamage)
+{
+    target.takeDamage(0.5 + (spellDamage + inventory.getSpellModifier() / 100 * spellDamage));
+}
+
+void Player::takeDamage(int damage)
+{
+    setHealth(0.5 + (getHealth() - damage - (double)(inventory.getArmorModifier() / 100 * damage)));
+    //health = health - (damage - inventory.getArmorModifier() / 100 * damage);
+}
+
+void Player::printInventory() const
+{
+    inventory.print();
+}
--- Dungeons and Dragons/player.hpp
+++ Dungeons and Dragons/player.hpp
@@ -4,17 +4,26 @@
 #include "entity.hpp"
 #include "race.hpp"
 #include "human.hpp"
-#include "mage.hpp"
-#include "warrior.hpp"
+#include "item.hpp"
+#include "inventory.hpp"
 
 class Player : public Entity
 {
 private:
     Race race;
+    Inventory inventory;
 public:
     Player(Race _race);
 
     std::string getRace() const;
+
+    void attack(Entity&, int attackDamage);
+
+    void castSpell(Entity&, int spellDamage);
+
+    void takeDamage(int damage);
+
+    void printInventory() const;
 
 };
 
