dd5f193ffb1a250e9690ebe4500547906611f20e
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-05-30 02:27:28 +0300

    The newly created class Matrix is now responsible for proper memory deallocation

--- Dungeons and Dragons/map.cpp
+++ Dungeons and Dragons/map.cpp
@@ -4,114 +4,33 @@
 #include <sstream>
 
 
-Map::Map(int _level/*, int _height, int _width, int _monsters, int _treasures*/) : level(_level)/*, height(_height), width(_width), monsters(_monsters), treasures(_treasures),*/
+Map::Map(int _level) : level(_level)
 {
-    map = nullptr;
     std::stringstream levelFileStream;
-    levelFileStream << "level" << level << ".txt";
+    levelFileStream << prefix << level << suffix;
     std::string fileName = levelFileStream.str();
     std::ifstream levelFile(fileName,std::ios::in);
+    int height, width;
     levelFile >> height >> width >> monsters >> treasures;
-
-    delete[] map;
-    map = new char* [height]{};
-    for (int i{0}; i < height; i++)
+    map = Matrix(height, width);
+    if (level >= 2)
     {
-        map[i] = new char[width]{};
+        generateNextFile();
     }
 }
-
-Map::Map(const Map& other) : level(other.level), height(other.height), width(other.width), monsters(other.monsters), treasures(other.treasures), map(nullptr)
-{
-    delete[] map;
-    map = new char* [other.height];
-    for (int i{0}; i < height; i++)
-    {
-        map[i] = new char[other.width];
-    }
-    for (int i{0}; i < height; i++)
-    {
-        for (int j{0}; j < width; j++)
-        {
-            map[i][j] = other.map[i][j];
-        }
-    }
-}
-
-Map& Map::operator=(const Map& other)
-{
-    if (this != &other)
-    {
-        level = other.level;
-        height = other.height;
-        width = other.width;
-        monsters = other.monsters;
-        treasures = other.treasures;
-        delete[] map;
-        map = new char* [other.height];
-        for (int i{0}; i < height; i++)
-        {
-            map[i] = new char[other.width];
-        }
-        for (int i{0}; i < height; i++)
-        {
-            for (int j{0}; j < width; j++)
-            {
-                map[i][j] = other.map[i][j];
-            }
-        }
-    }
-    return *this;
-}
-
-Map::~Map()
-{
-    for (int i{0}; i < height; i++)
-    {
-        delete[] map[i];
-    }
-    delete[] map;
-}
-
-/*
-int generateFile(int level)
-{
-    if (level <= 2)
-    {
-        int height1, width1, monsters1, treasures1;
-        std::ifstream level1File(level1Name, std::ios::in);
-        level1File >> height1 >> width1 >> monsters1 >> treasures1;
-        level1File.close();
-        int height2, width2, monsters2, treasures2;
-        std::ifstream level2File(level2Name, std::ios::in);
-        level2File >> height2 >> width2 >> monsters2 >> treasures2;
-        level2File.close();
-        return level;
-    }
-    else
-    {
-        std::stringstream constrcutName;
-        constrcutName << "level" << level << ".txt";
-        std::string newFileName;
-        newFileName = constrcutName.str();
-        std::ofstream(newFileName, std::ios::app);
-
-    }
-}
-*/
 
 void Map::generateNextFile() const
 {
-    if (this->getLevel() >= 2)
+    if (getLevel() >= 2)
     {
         int current[5]{getLevel(), getHeight(), getWidth(), getMonsters(), getTreasures()};
         int previous[5]{getLevel() - 1};
         std::stringstream currentNameStream;
         std::stringstream previousNameStream;
         std::stringstream nextNameStream;
-        nextNameStream << "level" << current[0] + 1 << ".txt";
-        currentNameStream << "level" << current[0] << ".txt";
-        previousNameStream << "level" << current[0] - 1 << ".txt";
+        nextNameStream << prefix << current[0] + 1 << suffix;
+        currentNameStream << prefix << current[0] << suffix;
+        previousNameStream << prefix << current[0] - 1 << suffix;
         std::string nextName = nextNameStream.str();
         std::string currentName = currentNameStream.str();
         std::string previousName = previousNameStream.str();
--- Dungeons and Dragons/map.hpp
+++ Dungeons and Dragons/map.hpp
@@ -2,44 +2,32 @@
 #define __MAP_HPP
 
 #include <string>
+#include "matrix.hpp"
 
 const std::string level1Name = "level1.txt";
 const std::string level2Name = "level2.txt";
-
+const std::string prefix = "level";
+const std::string suffix = ".txt";
 
 class Map
 {
 private:
-    //----------------Level 1----------------//
-    //static const int level1Height{10};
-    //static const int level1Width{10};
-    //static const int level1Monsters{2};
-    //static const int level1Treasures{2};
-    //----------------Level 2----------------//
-    //static const int level2Height{15};
-    //static const int level2Width{10};
-    //static const int level2Monsters{3};
-    //static const int level2Treasures{2};
-    //---------------------------------------//
     int level;
-    int height, width, monsters, treasures;
-    char** map;
+    int  monsters, treasures;
+    Matrix map;
+    void generateNextFile() const;
 public:
-    Map(int level = 0/*, int _height = 0, int _width = 0, int _monsters = 0, int _treasures = 0*/);
-
-    Map(const Map&);
-
-    Map& operator=(const Map&);
-
-    ~Map();
+    Map(int level = 0);
 
     int getLevel() const { return level; }
-    int getHeight() const { return height; }
-    int getWidth() const { return width; }
-    int getMonsters() const { return monsters; }
-    int getTreasures() const { return treasures; }
 
-    void generateNextFile() const;
+    int getHeight() const { return map.getColumns(); }
+
+    int getWidth() const { return map.getRows(); }
+
+    int getMonsters() const { return monsters; }
+
+    int getTreasures() const { return treasures; }
 };
 
 #endif // !__MAP_HPP
