7040fa4ebd6922c8cd1bbb6c903e05160fd11a6c
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-05-31 17:10:36 +0300

    Deleted useless comments

--- Dungeons and Dragons/treasure.cpp
+++ Dungeons and Dragons/treasure.cpp
@@ -1,14 +1,8 @@
-#include <random>
 #include "treasure.hpp"
 #include "weapon.hpp"
 #include "armor.hpp"
 #include "spell.hpp"
 
-/*
-
-std::random_device rd;
-std::mt19937 gen(rd());
-*/
 Treasure::Treasure()
 {
     //int num{std::uniform_int_distribution<>(0, 2)(gen)};
