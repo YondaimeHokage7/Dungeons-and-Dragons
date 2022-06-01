061c3c13bd45fd7c36a1a1132eb2a7dc49234134
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-06-01 17:20:45 +0300

    Addded a function that checks whether or not two cells are connected

--- Dungeons and Dragons/functions.cpp
+++ Dungeons and Dragons/functions.cpp
@@ -8,3 +8,28 @@ int randomBetween(int num1, int num2)
 {
     return std::uniform_int_distribution<>(num1, num2)(gen);
 }
+
+bool areConnected(CellIndex cell1, CellIndex cell2, CellIndex* connections, int size)
+{
+    bool result{false};
+    for (int i{0}; i < size && !result; i++)
+    {
+        if (cell1 == connections[i])
+        {
+            if (i < size - 1 && i > 0)
+            {
+                result = connections[i + 1] == cell2 || connections[i - 1] == cell2;
+            }
+            else if (i < size - 1)
+            {
+                result = connections[i + 1] == cell2;
+            }
+            else if (i > 0)
+            {
+                result = connections[i - 1] == cell2;
+            }
+        }
+    }
+    return result;
+}
+//bool isConnected(Stack* stack, CellIndex start, CellIndex end)
--- Dungeons and Dragons/functions.hpp
+++ Dungeons and Dragons/functions.hpp
@@ -1,3 +1,6 @@
 #pragma once
+#include "cellIndex.hpp"
 
 int randomBetween(int num1, int num2);
+
+bool areConnected(CellIndex cell1, CellIndex cell2, CellIndex* connections, int size);
