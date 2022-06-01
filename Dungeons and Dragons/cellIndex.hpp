6c8ac9afcfd6b25c8fd66880dd3e3f3cf5217281
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-06-01 17:24:00 +0300

    Added functions getRow() and getColumn(). Overloaded operator==

--- Dungeons and Dragons/cellIndex.hpp
+++ Dungeons and Dragons/cellIndex.hpp
@@ -8,13 +8,24 @@ struct CellIndex
     CellIndex(int _row = 0, int _column = 0) : row(_row), column(_column)
     {}
 
+    int getRow() const{ return row; }
+
+    int getColumn() const { return column; }
+
     void addI() { ++row; }
 
     void addJ() { ++column; }
-    
+
+    bool operator==(CellIndex& other)
+    {
+        return row == other.row && column == other.column;
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const CellIndex& cindex)
     {
         return os << '(' << cindex.row << ',' << cindex.column << ')' << '\n';
     }
 };
+
+
 
