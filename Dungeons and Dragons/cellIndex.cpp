3de482093f635e9cb67770033f4b8b9ad54486a5
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-06-01 17:32:43 +0300

    CellIndex is now a class with int row and columns as private members. The operator<< function is no longer a friend function.

--- Dungeons and Dragons/cellIndex.cpp
+++ Dungeons and Dragons/cellIndex.cpp
@@ -1,1 +1,9 @@
 #include "cellIndex.hpp"
+
+CellIndex::CellIndex(int _row, int _column) : row(_row), column(_column)
+{}
+
+std::ostream& operator<<(std::ostream& os, const CellIndex& cindex)
+{
+    return os << '(' << cindex.getRow() << ',' << cindex.getColumn() << ')' << '\n';
+}
--- Dungeons and Dragons/cellIndex.hpp
+++ Dungeons and Dragons/cellIndex.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include <iostream>
 
-struct CellIndex
+class CellIndex
 {
+private:
     int row, column;
+public:
+    CellIndex(int _row = 0, int _column = 0);
 
-    CellIndex(int _row = 0, int _column = 0) : row(_row), column(_column)
-    {}
-
-    int getRow() const{ return row; }
+    int getRow() const { return row; }
 
     int getColumn() const { return column; }
 
@@ -16,16 +16,8 @@ struct CellIndex
 
     void addJ() { ++column; }
 
-    bool operator==(CellIndex& other)
-    {
-        return row == other.row && column == other.column;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const CellIndex& cindex)
-    {
-        return os << '(' << cindex.row << ',' << cindex.column << ')' << '\n';
-    }
+    bool operator==(CellIndex& other) { return row == other.row && column == other.column; }
 };
 
-
+std::ostream& operator<<(std::ostream& os, const CellIndex& cindex);
 
