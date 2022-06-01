c4d85fbea97aa8bbda213a2ab0cd900fd12bf65a
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-06-01 17:21:36 +0300

    Added print and printBorder functions

--- Dungeons and Dragons/map.cpp
+++ Dungeons and Dragons/map.cpp
@@ -24,7 +24,7 @@ void Map::generateNextFile() const
 {
     if (getLevel() >= 2)
     {
-        int current[5]{getLevel(), getHeight(), getWidth(), getMonsters(), getTreasures()};
+        int current[5]{getLevel(), getRows(), getColumns(), getMonsters(), getTreasures()};
         int previous[5]{getLevel() - 1};
         std::stringstream currentNameStream;
         std::stringstream previousNameStream;
@@ -47,4 +47,96 @@ void Map::generateNextFile() const
             nextLevel << current[i] + previous[i] << ' ';
         }
     }
+}
+
+void Map::printBorder() const
+{
+    //Upper border
+    for (int i{0}; i < map.getColumns() * 3 + (map.getColumns() - 1) + 2; i++)
+    {
+        std::cout << '#';
+    }
+    std::cout << '\n';
+}
+
+void Map::print() const
+{
+    printBorder();
+    Stack* paths = getPaths();
+    CellIndex* connections = new CellIndex[paths->getTop() + 1];
+    int size{paths->getTop()};
+    //Copies the elements from the stack to the CallIndex array
+    for (int i{0}; paths->getTop() >= 0; i++)
+    {
+        connections[i] = paths->pop();
+    }
+    //Prints the maze
+    for (int i{0}; i < map.getRows() - 1; i++)
+    {
+        std::cout << '#';//Prints the left border of the current row
+        //Checks whether two cells are connected and prints the appropriate symbol
+        for (int j{0}; j < map.getColumns() - 1; j++)
+        {
+            std::cout << ' ' << getMatrix(i, j) << ' ';
+            if (areConnected(CellIndex(i, j), CellIndex(i, j + 1), connections, size))
+            {
+
+                std::cout << ' ';
+            }
+            else
+            {
+                std::cout << '#';
+            }
+        }
+        std::cout << " . #\n"; //Prints the last element of the row
+        std::cout << '#'; //prints the left border of the current border row
+        //Prints the current border row
+        for (int j{0}; j < map.getColumns() - 1; j++)
+        {
+            if (areConnected(CellIndex(i, j), CellIndex(i + 1, j), connections, size))
+            {
+                //Prints the appropriate sequence of characters depending on the borders between neighbouring cells
+                if (!areConnected(CellIndex(i, j), CellIndex(i, j + 1), connections, size) || !areConnected(CellIndex(i, j), CellIndex(i + 1, j + 1), connections, size))
+                {
+                    std::cout << "   #";
+                }
+                else
+                {
+                    std::cout << "    ";
+                }
+            }
+            else
+            {
+                std::cout << "####";
+            }
+        }
+        //Prints the last row of the map
+        if (areConnected(CellIndex(i, map.getColumns() - 1), CellIndex(i + 1, map.getColumns() - 1), connections, size))
+        {
+            std::cout << "   ";
+        }
+        else
+        {
+            std::cout << "###";
+        }
+        std::cout << '#';
+        std::cout << '\n';
+    }
+    std::cout << '#';
+    //Prints the last row
+    for (int i{0}; i < map.getColumns() - 1; i++)
+    {
+        int currentRow{map.getRows() - 1};
+        std::cout << ' ' << map.getMatrix(currentRow, i) << ' ';
+        if (areConnected(CellIndex(currentRow, i), CellIndex(currentRow, i + 1), connections, size))
+        {
+            std::cout << ' ';
+        }
+        else
+        {
+            std::cout << '#';
+        }
+    }
+    std::cout << " . #\n"; //prints the last element
+    printBorder();
 }
--- Dungeons and Dragons/map.hpp
+++ Dungeons and Dragons/map.hpp
@@ -16,20 +16,27 @@ private:
     int  monsters, treasures;
     Matrix map;
     void generateNextFile() const;
+    void printBorder() const;
+
 public:
     Map(int level = 0);
 
     int getLevel() const { return level; }
 
-    int getHeight() const { return map.getColumns(); }
+    int getColumns() const { return map.getColumns(); }
 
-    int getWidth() const { return map.getRows(); }
+    int getRows() const { return map.getRows(); }
+
+    char getMatrix(int i, int j) const { return map.getMatrix(i, j); }
 
     int getMonsters() const { return monsters; }
 
     int getTreasures() const { return treasures; }
 
-    Stack* getPaths() { return map.generatePaths(); }
+    Stack* getPaths() const { return map.generatePaths(); }
+
+    void print() const;
+
 };
 
 #endif // !__MAP_HPP
