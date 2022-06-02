a2990e60042669a74bc8610973fcc4c43f3209db
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-06-02 18:11:59 +0300

    Added myStrlen and myStrcmp functions

--- Dungeons and Dragons/functions.cpp
+++ Dungeons and Dragons/functions.cpp
@@ -9,6 +9,33 @@ int randomBetween(int num1, int num2)
     return std::uniform_int_distribution<>(num1, num2)(gen);
 }
 
+int myStrlen(char* string)
+{
+    int counter{0};
+    for (int i{0}; string[i] != '\0'; i++)
+    {
+        counter++;
+    }
+    return counter;
+}
+
+bool myStrcmp(char* string1, char* string2)
+{
+    bool result{true};
+    if (myStrlen(string1) != myStrlen(string2))
+    {
+        result = false;
+    }
+    for (int i{0}; string1[i] != '\0' && string2[i] != '\0' && result; i++)
+    {
+        if (string1[i] != string2[i])
+        {
+            result = false;
+        }
+    }
+    return result;
+}
+
 bool areConnected(CellIndex cell1, CellIndex cell2, CellIndex* connections, int size)
 {
     bool result{false};
