26f58943bf8ebb2a45aae6b1f8436c493fd9d2ac
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-06-02 18:13:27 +0300

    Added up, down, left and right functions

--- Dungeons and Dragons/cellIndex.hpp
+++ Dungeons and Dragons/cellIndex.hpp
@@ -17,6 +17,14 @@ public:
     void addJ() { ++column; }
 
     bool operator==(CellIndex& other) { return row == other.row && column == other.column; }
+
+    CellIndex up() const { return CellIndex(row - 1, column); }
+
+    CellIndex down() const { return CellIndex(row + 1, column); }
+
+    CellIndex left() const { return CellIndex(row, column - 1); }
+
+    CellIndex right() const { return CellIndex(row, column + 1); }
 };
 
 std::ostream& operator<<(std::ostream& os, const CellIndex& cindex);
