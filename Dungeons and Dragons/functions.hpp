d7cc609e9cb34efa7479aeabbd2c8727fc94f452
Hristo Valev <51285393+YondaimeHokage7@users.noreply.github.com>
2022-06-09 00:34:59 +0300

    myStrcmp now receives const char* as arguments

--- Dungeons and Dragons/functions.hpp
+++ Dungeons and Dragons/functions.hpp
@@ -5,7 +5,7 @@ int randomBetween(int num1, int num2);
 
 int myStrlen(const char* string);
 
-bool myStrcmp(char* string1, char* string2);
+bool myStrcmp(const char* string1, const char* string2);
 
 bool areConnected(CellIndex cell1, CellIndex cell2, CellIndex* connections, int size);
 
